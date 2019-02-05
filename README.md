4debc233a3712816348c83af54edb294e075520c
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2019-02-05 14:16:33 +0100

    Gitlabci (#1)
    Use GitlabCI for Continuous Integration
    
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- README.md
+++ README.md
@@ -4,21 +4,47 @@
 
 Terraforming ...
 
-## Kubernetes
+## Backend
+
+### Amazon S3
+
+Create a bucket:
+
+    $ aws s3api create-bucket --bucket my-galactus --region eu-west-1 \
+        --create-bucket-configuration LocationConstraint=eu-west-1
+
+Enable the server side encryption:
+
+    $ aws s3api put-bucket-encryption --bucket my-galactus \
+        --server-side-encryption-configuration={\"Rules\":[{\"ApplyServerSideEncryptionByDefault\":{\"SSEAlgorithm\":\"AES256\"}}]}
+
+Enable the bucket’s versioning:
+
+    $ aws s3api put-bucket-versioning --bucket my-galactus \
+        --versioning-configuration Status=Enabled
+
+### Google Cloud Storage
+
+*TODO*
+
+
+## Actions
+
+### Kubernetes
 
 * [ ] Kubernetes Cluster in Google Kubernetes Engine (GKE)
 * [ ] Kubernetes Cluster in Azure Container Service (AKS)
 
-## Source code
+### Source code
 
 * [x] Define your projects, organizations, teams on Github
 * [x] Define your projects, groups on Gitlab
 
-## Meetings
+### Meetings
 
 * [x] Google Calendar
 
-## Monitoring
+### Monitoring
 
 * [x] Updown
 * [x] UptimeRobot
--- github/foo/variables.tf
+++ github/foo/variables.tf
@@ -1,4 +1,4 @@
-# Copyright (C) 2018 Nicolas Lamirault <nicolas.lamirault@gmail.com>
+# Copyright (C) 2018-2019 Nicolas Lamirault <nicolas.lamirault@gmail.com>
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,39 +22,3 @@ variable "github_organization" {
   description = "(Optional) This is the target GitHub organization to manage."
 }
 
-variable "s3_bucket" {
-  type        = "string"
-  description = "(Optional) The name of the S3 bucket."
-  default     = "galactus"
-}
-
-variable "s3_key" {
-  type        = "string"
-  description = "(Optional) The path to the state file inside the bucket."
-}
-
-variable "s3_region" {
-  type        = "string"
-  description = "(Optional) The region of the S3 bucket."
-}
-
-variable "s3_endpoint" {
-  type        = "string"
-  description = "(Optional) A custom endpoint for the S3 API."
-}
-
-variable "consul_scheme" {
-  type        = "string"
-  description = "(Optional) Specifies what protocol to use when talking to the given address"
-  default     = "https"
-}
-
-variable "consul_address" {
-  type        = "string"
-  description = "DNS name and port of your Consul endpoint."
-}
-
-variable "consul_path" {
-  type        = "string"
-  description = "Path in the Consul KV store."
-}
